e02416667cd691fade7feb6f217397797a01aeef
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2024-03-19 23:30:10 -0400

    Test

--- src/boot.asm
+++ src/boot.asm
@@ -1,12 +1,31 @@
-bits 32	
+; Declare constants for the multiboot header.
+MBALIGN  equ  1 << 0            ; align loaded modules on page boundaries
+MEMINFO  equ  1 << 1            ; provide memory map
+MBFLAGS  equ  MBALIGN | MEMINFO ; this is the Multiboot 'flag' field
+MAGIC    equ  0x1BADB002        ; 'magic number' lets bootloader find the header
+CHECKSUM equ -(MAGIC + MBFLAGS)   ; checksum of above, to prove we are multiboot
+ 
+section .multiboot
+align 4
+	dd MAGIC
+	dd MBFLAGS
+	dd CHECKSUM
+
+section .bss
+align 16
+stack_bottom:
+resb 16384 ; 16 KiB
+stack_top:
+
 section .text
-	align 4
-	dd 0x1BADB002
-	dd 0x00
-	dd - (0x1BADB002 + 0x00)
-global start
+global _start:function (_start.end - _start)
 extern kernel_main
-start:
+_start:
+	mov esp, stack_top
+ 
 	cli
 	call kernel_main
 	hlt
+.hang:	hlt
+	jmp .hang
+.end:
